3b0bc71482806c3ed280d56e1316f6f6ebe3ff15
RodrigoAGM <rod-gm@hotmail.com>
2019-06-16 16:55:23 -0500

    Project setup

---
+++ projectionTest/src/test/java/io/proyection/projectiontest/ProjectiontestApplicationTests.java
@@ -0,0 +1,16 @@
+package io.proyection.projectiontest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectiontestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
